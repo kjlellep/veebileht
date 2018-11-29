d9e20758c383b70c32d8cf1318337aa01702825a
rlellep <sonyw200@hotmail.com>
2018-11-29 19:38:10 +0200

    ei oska js lisada

---
+++ nxt.js
@@ -0,0 +1,11 @@
+function inter() {
+    document.getElementById("trol").style.fontSize = "xx-large";
+    document.getElementById("trol2").style.fontSize = "xx-large";
+    document.getElementById("trol3").style.fontSize = "xx-large";
+}
+function beck() {
+    document.getElementById("trol").style.fontSize = "xx-small";
+    document.getElementById("trol2").style.fontSize = "xx-small";
+    document.getElementById("trol3").style.fontSize = "xx-small";
+    document.getElementById("next").style.fontSize = "xx-small";
+}
