02ed53267951c17d97bc8a9b58c82ee563bdbd6e
rlellep <sonyw200@hotmail.com>
2018-11-30 10:07:23 +0200

    kirjaveaparandused

--- nxt.js
+++ nxt.js
@@ -2,6 +2,7 @@ function inter() {
     document.getElementById("trol").style.fontSize = "xx-large";
     document.getElementById("trol2").style.fontSize = "xx-large";
     document.getElementById("trol3").style.fontSize = "xx-large";
+    document.getElementById("next").style.fontSize = "xx-large";
 }
 function beck() {
     document.getElementById("trol").style.fontSize = "xx-small";
