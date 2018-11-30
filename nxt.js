95951e322e8f3d0e1e354613c12b440065833aff
rlellep <sonyw200@hotmail.com>
2018-11-30 13:01:31 +0200

    final touch

--- nxt.js
+++ nxt.js
@@ -1,12 +1,14 @@
 function inter() {
-    document.getElementById("trol").style.fontSize = "xx-large";
-    document.getElementById("trol2").style.fontSize = "xx-large";
-    document.getElementById("trol3").style.fontSize = "xx-large";
-    document.getElementById("next").style.fontSize = "xx-large";
-}
-function beck() {
     document.getElementById("trol").style.fontSize = "xx-small";
     document.getElementById("trol2").style.fontSize = "xx-small";
     document.getElementById("trol3").style.fontSize = "xx-small";
     document.getElementById("next").style.fontSize = "xx-small";
+    document.body.style.background = "black";
+}
+function beck() {
+    document.getElementById("trol").style.fontSize = "medium";
+    document.getElementById("trol2").style.fontSize = "medium";
+    document.getElementById("trol3").style.fontSize = "medium";
+    document.getElementById("next").style.fontSize = "xx-large";
+    document.body.style.background = "rgba(0,255,0,0.4)"
 }
